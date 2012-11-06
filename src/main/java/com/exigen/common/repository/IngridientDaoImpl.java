1c7c7dbe7fee520193302fcaa6b8dba92d77f263
ivanguravel <ivanguravel@code.google.com>
2012-11-06 12:56:47 +0200

    Task #796 "Modify db structure(add steps, ingridients, calories)". Add Dao "Ingridient" entity

---
+++ src/main/java/com/exigen/common/repository/IngridientDaoImpl.java
@@ -0,0 +1,51 @@
+package com.exigen.common.repository;
+
+
+import com.exigen.common.domain.Ingridient;
+import com.exigen.common.domain.Recipe;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * {@inheritDoc}
+ */
+@Repository("ingridientDao")
+public class IngridientDaoImpl implements IngridientDao {
+
+    /**
+     * {@code entityManager} describes the EntityManager for JPA ORM
+     */
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Ingridient> getIngridientsRecipeList(Recipe recipe) {
+        List<Ingridient> ingridientList = this.entityManager.createQuery("SELECT t FROM Ingridient t WHERE t.recipe=:selectedRecipe")
+                .setParameter("selectedRecipe", recipe)
+                .getResultList();
+
+        return ingridientList;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addIngridient(Ingridient ingridient) {
+        entityManager.persist(ingridient);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeIngridient(Ingridient ingridient) {
+        entityManager.remove(ingridient);
+    }
+}
