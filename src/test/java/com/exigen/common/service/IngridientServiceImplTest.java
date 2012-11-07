b943f4ca474595434ccbb734eceaa190d1f3d36f
ivanguravel <ivanguravel@code.google.com>
2012-11-07 19:53:29 +0200

    Task #796 "Modify db structure(add steps, ingridients, calories)". Add test for service and entity

---
+++ src/test/java/com/exigen/common/service/IngridientServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.exigen.common.service;
+
+
+import com.exigen.common.domain.Recipe;
+import com.exigen.common.repository.IngridientDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static junit.framework.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+public class IngridientServiceImplTest {
+
+    IngridientServiceImpl ingridientService = new IngridientServiceImpl();
+
+    Recipe recipe = new Recipe();
+
+    @Mock
+    private IngridientDao ingridientDao;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getIngridientService() {
+        ingridientService.setIngridientDao(ingridientDao);
+        when(ingridientDao.getIngridientsRecipeList(recipe)).thenReturn(null);
+        assertNull(ingridientService.getIngridientsRecipeList(recipe));
+    }
+
+}
