fb80d8cc535b0b6c15826a7ba77b0e4350b8f1c1
TonySoprano <TonySoprano@TonySopranoPK>
2012-11-10 18:40:52 +0200

    added title of cuisine instead of "???????"

--- src/main/java/com/exigen/common/web/MenuController.java
+++ src/main/java/com/exigen/common/web/MenuController.java
@@ -48,8 +48,9 @@ public class MenuController {
     @RequestMapping(value = {"/recipeListFromAjax"})
     @ResponseBody
     public ModelAndView listRecipesToAjax(@RequestParam("cuisineId") Integer cuisineId) {
-
-        return new ModelAndView("recipeListFromAjax", "model", this.cuisineService.getOneCuisine(cuisineId));
+        ModelAndView mav = new ModelAndView("recipeListFromAjax", "model", this.cuisineService.getOneCuisine(cuisineId));
+        mav.addObject("cuisineTitle", this.cuisineService.getCuisine().get(cuisineId-1).getCuisin());
+        return mav;
     }
 
 }
