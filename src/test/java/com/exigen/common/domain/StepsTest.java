7fc6ebcb2c220f1f00c39853d7a1a918a872b790
ivanguravel <ivanguravel@code.google.com>
2012-11-11 16:38:44 +0200

    Task #796 "Modify db structure(add steps, ingridients, calories)". Add new test for Steps entity

--- src/test/java/com/exigen/common/domain/StepsTest.java
+++ src/test/java/com/exigen/common/domain/StepsTest.java
@@ -12,17 +12,32 @@ public class StepsTest {
 
     @Test
     public void testGetSetNameOfStep(){
-       assertNull(steps.getNameOfStep());
-        steps.setNameOfStep("step");
-        assertEquals("step",steps.getNameOfStep());
+       assertNull(steps.getStepDescription());
+        steps.setStepDescription("step");
+        assertEquals("step",steps.getStepDescription());
     }
 
     @Test
     public void testGetSetRecipe(){
         Recipe recipe=new Recipe();
-        assertNull(steps.getNameOfStep());
+        assertNull(steps.getRecipe());
         steps.setRecipe(recipe);
         assertEquals(recipe, steps.getRecipe());
+    }
+
+    @Test
+    public void testGetSetStepNumber(){
+        assertEquals(null,steps.getStepNumber());
+        Integer n=12;
+        steps.setStepNumber(n);
+        assertEquals(n,steps.getStepNumber());
+    }
+
+    @Test
+    public void testGetSetStepImg(){
+        assertNull(steps.getStepImg());
+        steps.setStepImg("/img/ukr.jpg");
+        assertEquals("/img/ukr.jpg", steps.getStepImg());
     }
 
 }
