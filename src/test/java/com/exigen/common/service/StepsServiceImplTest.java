f1cdf6ede5d3fac60eb2a4d95210d02375b65fd5
ivanguravel <ivanguravel@code.google.com>
2012-11-11 14:38:25 +0200

    Task #796 "Modify db structure(add steps, ingridients, calories)". Add test for service StepsService

---
+++ src/test/java/com/exigen/common/service/StepsServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.exigen.common.service;
+
+
+import com.exigen.common.domain.Recipe;
+import com.exigen.common.repository.StepsDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+public class StepsServiceImplTest {
+
+    private StepsServiceImpl stepsService=new StepsServiceImpl();
+
+    Recipe recipe = new Recipe();
+
+    @Mock
+    StepsDao stepsDao;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getStepsService() {
+        stepsService.setStepsDao(stepsDao);
+        when(stepsDao.getStepsRecipeList(recipe)).thenReturn(null);
+        assertNull(stepsDao.getStepsRecipeList(recipe));
+    }
+}
