db3f472f02124e0624f97c180e6d240d08e37735
Dimyc <Dimyc@Dimyc-Pikyc>
2012-11-15 22:10:05 +0200

    Update Recipe entity, its dao and service. (make search by recipeId method return only one recipe, instead of a list of recipes).

--- src/main/java/com/exigen/common/service/RecipeService.java
+++ src/main/java/com/exigen/common/service/RecipeService.java
@@ -17,7 +17,7 @@ public interface RecipeService {
     /**
      * {@method getRecipeCuisineList(Cuisine cuisine)}
      *
-     * @param cuisine(object of some particular recipe)
+     * @param cuisine (object of some particular recipe)
      * @return the list of recipes, where cuisine in Recipe entity =@param).
      * @throws org.springframework.dao.DataAccessException(resource
      *                                   on cloudfoundry is unavalible, DB is changed)
@@ -29,21 +29,21 @@ public interface RecipeService {
     /**
      * {@method getOneRecipeList(Integer recipeId)}
      *
-     * @param recipeId(unique identificator of some particular recipe)
-     * @return the list of recipes, where Recipe field recipeId equals parameter recipeId( return complete description
+     * @param recipeId (unique identificator of some particular recipe)
+     * @return the recipe, where Recipe field recipeId equals parameter recipeId( return complete description
      *         of the recipe with the steps, ingredients, Pictures.).
      * @throws org.springframework.dao.DataAccessException(resource
      *                                   on cloudfoundry is unavalible, DB is changed)
      * @throws NullPointerException(when recipeId is null, or id has no results in the database)
      */
 
-    List<Recipe> getOneRecipeList(Integer recipeId);
+    Recipe getOneRecipe(Integer recipeId);
 
     /**
      * {@method addRecipe(Recipe recipe)}
      * for adding information about some particular recipe into DB
      *
-     * @param recipe(object of some particular recipe)
+     * @param recipe (object of some particular recipe)
      * @throws org.springframework.dao.DataAccessException(resource
      *                                   on cloudfoundry is unavalible, DB is changed)
      * @throws NullPointerException(when recipe is null)
@@ -54,7 +54,7 @@ public interface RecipeService {
      * {@method removeRecipe(Recipe recipe)}
      * for remove some particular recipe from DB
      *
-     * @param recipe(object of some particular recipe)
+     * @param recipe (object of some particular recipe)
      * @throws org.springframework.dao.DataAccessException(resource
      *                                   on cloudfoundry is unavalible, DB is changed)
      * @throws NullPointerException(when recipe is null)
