c4ed359d8e25424a9827f70c87b590c0dddba050
Elena <vev77777@gmail.com>
2013-01-24 17:31:37 +0200

    Task #912 Create view for Login page

---
+++ src/main/java/com/exigen/common/web/LoginController.java
@@ -0,0 +1,22 @@
+package com.exigen.common.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/**
+ * Class  {@code LoginController} creates login
+ * freemarker pages
+ *
+ * @author Elena.
+ * date: January 24, 2013
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login")
+
+    public ModelAndView login(){
+      return new ModelAndView("loginForm");
+     }
+}
