992cb4b63be48beedea61eeb721dc8c391e98895
ivanguravel <ivanguravel@code.google.com>
2013-01-30 20:50:55 +0200

    Task #941 "MORE Code Coverage". add tests

---
+++ src/test/java/com/exigen/common/controller/RegistrationControllerTest.java
@@ -0,0 +1,23 @@
+package com.exigen.common.controller;
+
+import com.exigen.common.web.RegistrationController;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 30.01.13
+ * Time: 20:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegistrationControllerTest {
+    @Test
+    public void testRegistration() throws Exception {
+        RegistrationController registrationController=new RegistrationController();
+        String test=registrationController.registration();
+
+        Assert.assertEquals("registration",test);
+
+    }
+}
