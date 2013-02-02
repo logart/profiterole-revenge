ed8d706b30a3bd9584d96b3047bd29fd77e57396
ivanguravel <ivanguravel@code.google.com>
2013-02-02 15:55:43 +0200

    add new tests

--- src/test/java/com/exigen/common/service/IngridientServiceTest.java
+++ src/test/java/com/exigen/common/service/IngridientServiceTest.java
@@ -1,6 +1,7 @@
 package com.exigen.common.service;
 
 
+import com.exigen.common.domain.Ingridient;
 import com.exigen.common.domain.Recipe;
 import com.exigen.common.repository.IngridientDao;
 import junit.framework.Assert;
@@ -8,6 +9,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -18,6 +22,15 @@ public class IngridientServiceTest {
 
     Recipe recipe = new Recipe();
 
+     List<Integer> recipesId=new ArrayList<Integer>();
+
+
+    Ingridient ingridient=new Ingridient("ingr",8,5,"unit",recipe);
+    Ingridient ingridientTwo = new Ingridient("ingr2",7,3,"un",recipe);
+        
+    List<Ingridient> ingridients=new ArrayList<Ingridient>();
+    
+    
     @Mock
     IngridientDao ingridientDao;
 
@@ -27,10 +40,34 @@ public class IngridientServiceTest {
     }
 
     @Test
-    public void getIngridientsService() {
+    public void getIngridientsServiceTest() {
         ingridientService.setIngridientDao(ingridientDao);
         when(ingridientDao.getIngridientsRecipeList(recipe.getRecipeId())).thenReturn(null);
         Assert.assertNull(ingridientService.getIngridientsRecipeList(recipe.getRecipeId()));
+    }
+
+    @Test
+    public void getAllIngridientsWithOutRecipesInjTest(){
+        ingridients.add(ingridient);
+        ingridients.add(ingridientTwo);
+        ingridientService.setIngridientDao(ingridientDao);
+        when(ingridientDao.getAllIngridients()).thenReturn(ingridients);
+        Assert.assertEquals(ingridients,ingridientDao.getAllIngridients());
+    }
+
+    @Test
+    public void getIngridientByIdTest(){
+        when(ingridientDao.getIngridientById(ingridient.getIngridientId())).thenReturn(ingridient);
+        ingridientService.setIngridientDao(ingridientDao);
+        Assert.assertEquals(ingridient,ingridientDao.getIngridientById(ingridient.getIngridientId()));
+    }
+
+    public void getAllIngridientsTest(){
+     recipesId.add(recipe.getRecipeId());
+        when(ingridientDao.getIngridientsRecipeList(recipesId.get(0))).thenReturn(ingridients);
+        ingridientService.setIngridientDao(ingridientDao);
+        Assert.assertEquals(8,ingridientService.getAllIngridients(recipesId).get(0).getCountOfIngridient());
+
     }
 
 
