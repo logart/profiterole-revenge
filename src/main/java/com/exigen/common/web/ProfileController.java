0a270ae09c536108d0a70add09455efdbe0440cb
gioia <gioia@localhost>
2013-03-15 22:39:54 +0200

    profile view

---
+++ src/main/java/com/exigen/common/web/ProfileController.java
@@ -0,0 +1,49 @@
+package com.exigen.common.web;
+
+import com.exigen.common.domain.Account;
+import com.exigen.common.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Map;
+
+
+/**
+ * Class  {@code ProfileController} is used to handle
+ *  requests for profile information view
+ * @autor Victoria Ganzha.
+ * @Date 12.03.13
+ *
+ */
+
+@Controller
+@Secured("ROLE_USER")
+@RequestMapping("/profile")
+public class ProfileController{
+
+    /**
+     * {@code accountService} describes the AccountService to inject on this
+     * class
+     */
+
+    @Autowired
+    private AccountService accountService;
+
+    /**
+     * {@method viewProfile() for authentication user }
+     *
+     * @return information about profile
+     */
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewProfile(Map model){
+        Account account =  accountService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        model.put("account", account);
+        return "profile";
+
+    }
+
+}
