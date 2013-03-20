a7e6af10e7d24676a216752675ce423052846d68
Elena Vasilkovich <vev77777@gmail.com>
2013-03-20 21:00:03 +0200

    CategoryTest and CuisineTest

--- src/test/java/com/exigen/common/domain/CuisineTest.java
+++ src/test/java/com/exigen/common/domain/CuisineTest.java
@@ -2,26 +2,48 @@ package com.exigen.common.domain;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CuisineTest {    //TODO
-//    Cuisine cuisine;
-//
-//    @Test
-//    public void testGetSetId() throws Exception {
-//        cuisine = new Cuisine();
-//        Assert.assertNull(cuisine.getCuisineId());
-//        Integer test = 2;
-//        cuisine.setCuisineId(test);
-//        Assert.assertEquals(test, cuisine.getCuisineId());
-//    }
-//
-//    @Test
-//    public void testGetSetCusine() throws Exception {
-//        cuisine = new Cuisine();
-//        Assert.assertNull(cuisine.getCuisin());
-//        String cuis = "Cuisine";
-//        cuisine.setCuisin(cuis);
-//        Assert.assertEquals(cuis, cuisine.getCuisin());
-//    }
+import static junit.framework.Assert.*;
 
+public class CuisineTest {
+
+    @Test
+    public void testGetSetId() throws Exception {
+        Cuisine    cuisine = new Cuisine();
+        assertNull(cuisine.getId());
+        Integer test = 2;
+        cuisine.setId(test);
+        Assert.assertEquals(test, cuisine.getId());
+    }
+
+    @Test
+    public void testGetSetName() throws Exception {
+        Cuisine cuisine = new Cuisine();
+        assertNull(cuisine.getName());
+        String name = "Cuisine";
+        cuisine.setName(name);
+        Assert.assertEquals(name, cuisine.getName());
+    }
+
+    @Test
+    public void testGetSetImage() throws Exception {
+        Cuisine cuisine = new Cuisine();
+        assertNull(cuisine.getImage());
+        String image = "Cuisine";
+        cuisine.setName(image);
+        Assert.assertEquals(image, cuisine.getImage());
+    }
+
+    @Test
+    public void testGetSetRecipeList(){
+        Cuisine cuisine = new Cuisine();
+        List<Recipe> recipeList=new ArrayList<Recipe>();
+        Recipe recipe=new Recipe();
+        recipeList.add(0,recipe);
+        assertNull(cuisine.getRecipeList());
+        cuisine.setRecipeList(recipeList);
+        Assert.assertEquals(recipeList, cuisine.getRecipeList());
+    }
 }
