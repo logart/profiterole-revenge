96043760da85bd0d1fbf6c6420b4839b199cabb1
Elena Vasilkovich <vev77777@gmail.com>
2013-03-21 23:07:01 +0200

    IngredientTest

--- src/test/java/com/exigen/common/domain/IngredientTest.java
+++ src/test/java/com/exigen/common/domain/IngredientTest.java
@@ -1,48 +1,50 @@
 package com.exigen.common.domain;
 
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 
-public class IngredientTest {                   //TODO
-//
-//    Ingredient ingredient = new Ingredient();
-//
-//    @Test
-//    public void testGetSetNameOfIngridient() {
-//        assertNull(ingredient.getNameOfIngridient());
-//        ingredient.setNameOfIngridient("ingredient");
-//        assertEquals("ingredient", ingredient.getNameOfIngridient());
-//    }
-//
-//    @Test
-//    public void testGetSetCalories() {
-//        assertEquals(ingredient.getCalories(), 0.0, 0.0);
-//        ingredient.setCalories(5);
-//        assertEquals(ingredient.getCalories(), 5, 5);
-//    }
-//
-//    @Test
-//    public void testGetSetRecipe() {
-//        Recipe recipe = new Recipe();
-//        assertNull(ingredient.getRecipe());
-//        ingredient.setRecipe(recipe);
-//        assertEquals(ingredient.getRecipe(), recipe);
-//
-//    }
-//
-//    @Test
-//    public void testGetSetCountOfIngridient() {
-//        assertEquals(0, ingredient.getCountOfIngridient());
-//        ingredient.setCountOfIngridient(10);
-//        assertEquals(10, ingredient.getCountOfIngridient());
-//    }
-//
-//    @Test
-//    public void testGetSetUnitOfMeasure(){
-//     assertNull(ingredient.getUnitOfMeasure());
-//        ingredient.setUnitOfMeasure("unit");
-//        assertEquals("unit", ingredient.getUnitOfMeasure());
-//    }
+public class IngredientTest {
+
+    Ingredient ingredient = new Ingredient();
+
+    @Test
+    public void testGetSetId() throws Exception {
+        assertNull(ingredient.getId());
+        Integer test = 2;
+        ingredient.setId(test);
+        assertEquals(test, ingredient.getId());
+
+    }
+    @Test
+    public void testGetSetName() throws Exception {
+        assertNull(ingredient.getName());
+        ingredient.setName("ingredient");
+        assertEquals("ingredient", ingredient.getName());
+    }
+
+    @Test
+    public void testGetSetCalories() throws Exception {
+        assertEquals(ingredient.getCalories(), 0.0, 0.0);
+        ingredient.setCalories(5);
+        assertEquals(ingredient.getCalories(), 5, 5);
+    }
+
+    @Test
+    public void testGetSetMeasuresBucketList() throws Exception {
+        Ingredient ingredient = new Ingredient();
+        assertEquals(true,ingredient.getMeasuresBucketList().isEmpty());
+
+        List<MeasuresBucket> measuresBucketList = new ArrayList<MeasuresBucket>();
+        MeasuresBucket measuresBucket = new MeasuresBucket();
+        measuresBucketList.add(0,measuresBucket);
+        ingredient.setMeasuresBucketList(measuresBucketList);
+        assertEquals(measuresBucketList,ingredient.getMeasuresBucketList());
+    }
+
 }
