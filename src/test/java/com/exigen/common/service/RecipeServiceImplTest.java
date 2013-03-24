c03016e225a828a6a762e93b87a20b024599512e
Samokhval Volodymyr <volodymyr.samokhval@gmail.com>
2013-03-24 20:26:06 +0200

    Remove unused method caloriesCalculation and add tests

--- src/test/java/com/exigen/common/service/RecipeServiceImplTest.java
+++ src/test/java/com/exigen/common/service/RecipeServiceImplTest.java
@@ -18,55 +18,40 @@ import static org.mockito.Mockito.when;
 
 public class RecipeServiceImplTest {        //TODO
 
-//    RecipeServiceImpl recipeService=new RecipeServiceImpl();
-//
-//    @Mock
-//    RecipeDao recipeDao;
-//
-//    Cuisine cuisine=new Cuisine();
-//
-//    Recipe recipe=new Recipe("title","desc",cuisine,new Category(),"http","smallImg",12,20);
-//    Recipe recipeTwo=new Recipe("title2","descr",cuisine,new Category(),"bigImg","smallImg",5,4);
-//
-//    List<Recipe> recipes=new ArrayList<Recipe>();
-//
-//    List<Integer> recipesId=new ArrayList<Integer>();
-//
-//
-//    @Before
-//    public void setup() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    public void getRecipeCuisineListTest(){
-//     recipes.add(recipe);
-//     recipes.add(recipeTwo);
-//     when(recipeDao.getRecipeCuisineList(cuisine)).thenReturn(recipes);
-//        recipeService.setRecipeDao(recipeDao);
-//        Assert.assertEquals(recipes,recipeDao.getRecipeCuisineList(cuisine));
-//    }
-//
-//     @Test
-//    public void getOneRecipeTest(){
-//         recipes.add(recipe);
-//         when(recipeDao.getOneRecipe(recipes.get(0).getRecipeId())).thenReturn(recipes.get(0));
-//         recipeService.setRecipeDao(recipeDao);
-//         Assert.assertEquals(recipe,recipeService.getOneRecipe(recipes.get(0).getRecipeId()));
-//     }
-//
-//    @Test
-//    public void caloriesCalculationTest(){
-//        recipesId.add(recipe.getRecipeId());
-//        recipesId.add(recipeTwo.getRecipeId());
-//        when(recipeDao.getOneRecipe(recipesId.get(0))).thenReturn(recipe);
-//        recipeService.setRecipeDao(recipeDao);
-//        Integer test=recipeService.caloriesCalculation(recipesId);
-//        when(recipeDao.getOneRecipe(recipesId.get(0))).thenReturn(recipeTwo);
-//        recipeService.setRecipeDao(recipeDao);
-//        Integer testTwo=recipeService.caloriesCalculation(recipesId);
-//        Assert.assertEquals(24,test+testTwo);
-//
-//
-//    }
+    RecipeServiceImpl recipeService=new RecipeServiceImpl();
+
+    @Mock
+    RecipeDao recipeDao;
+
+    Cuisine cuisine=new Cuisine();
+
+    Recipe recipe=new Recipe("title","desc",cuisine,new Category(),"http","smallImg",20);
+    Recipe recipeTwo=new Recipe("title2","descr",cuisine,new Category(),"bigImg","smallImg",30);
+
+    List<Recipe> recipes=new ArrayList<Recipe>();
+
+    List<Integer> recipesId=new ArrayList<Integer>();
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getRecipeCuisineListTest(){
+     recipes.add(recipe);
+     recipes.add(recipeTwo);
+     when(recipeDao.getRecipeCuisineList(cuisine)).thenReturn(recipes);
+        recipeService.setRecipeDao(recipeDao);
+        Assert.assertEquals(recipes,recipeDao.getRecipeCuisineList(cuisine));
+    }
+
+     @Test
+    public void getOneRecipeTest(){
+         recipes.add(recipe);
+         when(recipeDao.getOneRecipe(recipes.get(0).getId())).thenReturn(recipes.get(0));
+         recipeService.setRecipeDao(recipeDao);
+         Assert.assertEquals(recipe,recipeService.getOneRecipe(recipes.get(0).getId()));
+     }
 }
