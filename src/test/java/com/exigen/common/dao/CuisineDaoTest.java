f4ab5f2661740f8b77d1c1d4341376916a03b6ee
Elena Vasilkovich <vev77777@gmail.com>
2013-03-24 22:26:13 +0200

    CuisineDaoTest

--- src/test/java/com/exigen/common/dao/CuisineDaoTest.java
+++ src/test/java/com/exigen/common/dao/CuisineDaoTest.java
@@ -25,58 +25,56 @@ import java.util.List;
 @TransactionConfiguration(defaultRollback = true)
 @Transactional
 public class CuisineDaoTest {
-//
-//
-//    @Autowired
-//    private CuisineDao cuisineDao;
-//    @Autowired
-//    private RecipeDao recipeDao;
-//    @Autowired
-//    private CategoriesDao categoriesDao;
-//
-//    private Cuisine testCuisine;
-//
-//    private Category testCategory;
-//
-//    private Recipe testRecipe=new Recipe("title","desc",testCuisine, testCategory,"bigimg","smalling",5,5);
-//
-//    private List<Recipe> recipes=new ArrayList<Recipe>();
-//
-//    @Before
-//    public void setup() {
-//        testCuisine = new Cuisine("test", "test");
-//        testCategory =new Category("categ");
-//
-//    }
-//
-//
-//    @Test
-//    public void addCuisineTest() {
-//
-//
-//        cuisineDao.addCuisine(testCuisine);
-//        Assert.assertEquals(testCuisine, cuisineDao.getCuisine().get(0));
-//    }
-//
-//
-//    @Test
-//    public void getCategoriesTest() {
-//        Assert.assertNotNull(cuisineDao.getCuisine());
-//    }
-//
-//    @Test
-//    public void getOneCuisineListTest(){
-//        categoriesDao.addCategories(testCategory);
-//        cuisineDao.addCuisine(testCuisine);
-//        testRecipe.setCategory(testCategory);
-//        testRecipe.setCuisine(testCuisine);
-//        recipeDao.addRecipe(testRecipe);
-//        recipes.add(testRecipe);
-//        testCuisine.setRecipes(recipes);
-//        cuisineDao.addCuisine(testCuisine);
-//        Assert.assertNull(recipeDao.getOneRecipe(testCuisine.getCuisineId()));
-//
-//
-//    }
+
+
+    @Autowired
+    private CuisineDao cuisineDao;
+    @Autowired
+    private RecipeDao recipeDao;
+    @Autowired
+    private CategoriesDao categoriesDao;
+
+    private Cuisine testCuisine;
+
+    private Category testCategory;
+
+    private Recipe testRecipe=new Recipe("title","desc",testCuisine, testCategory,"bigimg","smalling",5);
+
+    private List<Recipe> recipes=new ArrayList<Recipe>();
+
+    @Before
+    public void setup() {
+        testCuisine = new Cuisine("test", "test");
+        testCategory =new Category("categ");
+
+    }
+
+
+    @Test
+    public void addCuisineTest() {
+        cuisineDao.addCuisine(testCuisine);
+        Assert.assertEquals(testCuisine, cuisineDao.getCuisine().get(0));
+    }
+
+
+    @Test
+    public void getCategoriesTest() {
+        Assert.assertNotNull(cuisineDao.getCuisine());
+    }
+
+    @Test
+    public void getOneCuisineListTest(){
+        categoriesDao.addCategories(testCategory);
+        cuisineDao.addCuisine(testCuisine);
+        testRecipe.setCategory(testCategory);
+        testRecipe.setCuisine(testCuisine);
+        recipeDao.addRecipe(testRecipe);
+        recipes.add(testRecipe);
+        testCuisine.setRecipeList(recipes);
+        cuisineDao.addCuisine(testCuisine);
+        Assert.assertNotNull(recipeDao.getOneRecipe(testCuisine.getId()));
+
+
+    }
 
 }
