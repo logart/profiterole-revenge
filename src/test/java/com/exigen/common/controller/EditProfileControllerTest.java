9b98eda7a07bbc52c0e4867d39fae2d7a9f6fac5
Sivn <Sivn@Sivn-PC>
2013-03-24 19:53:13 +0200

    EditProfileController cosmetics added EditProfileControllerTest

---
+++ src/test/java/com/exigen/common/controller/EditProfileControllerTest.java
@@ -0,0 +1,96 @@
+package com.exigen.common.controller;
+
+import com.exigen.common.domain.Account;
+import com.exigen.common.domain.AccountData;
+import com.exigen.common.service.AccountService;
+import com.exigen.common.web.EditProfileController;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.validation.BindingResult;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: A. Kondratiev
+ * Date: 22.03.13
+ * Using for testing EditProfileController
+ */
+public class EditProfileControllerTest {
+    @Mock
+    private AccountService accountService ;
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    public void testEditingProfileUnauthorized() throws Exception {
+        when(accountService.findByUsername("test")).thenReturn(new Account());
+
+        EditProfileController profile = new EditProfileController();
+        ReflectionTestUtils.setField(profile, "accountService", this.accountService);
+        Map model=new TreeMap();
+        Assert.assertNotNull(profile.editingProfile(model));
+        Assert.assertEquals("redirect:", profile.editingProfile(model) );
+    }
+
+    @Test
+    public void testEditingProfileAuthorized() throws Exception {
+        User user = new User("test","pass", AuthorityUtils.createAuthorityList("ROLE_USER"));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user,null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        when(accountService.findByUsername("test")).thenReturn(new Account());
+
+        EditProfileController profile = new EditProfileController();
+        ReflectionTestUtils.setField(profile, "accountService", this.accountService);
+        Map model=new TreeMap();
+        Assert.assertNotNull(profile.editingProfile(model));
+        Assert.assertEquals("editProfile", profile.editingProfile(model) );
+    }
+
+    @Test
+    public void testProcessEditingProfileValidatedOk(){
+        EditProfileController profile = new  EditProfileController();
+        ReflectionTestUtils.setField(profile, "accountService", this.accountService);
+
+        Map model = new TreeMap();
+        AccountData accountData= new AccountData() ;
+        BindingResult result = mock(BindingResult.class);
+        when(result.hasErrors()).thenReturn(false);
+
+        Assert.assertEquals("redirect:profile", profile.processEditingProfile(model,accountData,result));
+        verify(this.accountService).updateAccount((AccountData)anyObject());
+    }
+
+    @Test
+    public void testProcessEditingProfileValidatedNotOk(){
+        EditProfileController profile = new  EditProfileController();
+        ReflectionTestUtils.setField(profile, "accountService", this.accountService);
+
+        Map model = new TreeMap();
+        AccountData accountData= new AccountData() ;
+        BindingResult result = mock(BindingResult.class);
+        when(result.hasErrors()).thenReturn(true);
+
+        Assert.assertNotNull(profile.processEditingProfile(model,accountData,result));
+        Assert.assertEquals("editProfile", profile.processEditingProfile(model,accountData,result));
+
+    }
+}
