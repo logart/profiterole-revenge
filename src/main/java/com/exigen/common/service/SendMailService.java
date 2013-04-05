dd32c22778bd6d7ad0e1ed95c764d005264997ef
Elena Vasilkovich <vev77777@gmail.com>
2013-04-05 22:08:55 +0300

    Task 1086 Send Mail. Update SendMailService and add mail.ftl

--- src/main/java/com/exigen/common/service/SendMailService.java
+++ src/main/java/com/exigen/common/service/SendMailService.java
@@ -1,11 +1,23 @@
 package com.exigen.common.service;
 
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -17,30 +29,104 @@ import org.springframework.stereotype.Service;
  */
 @Service("sendMailService")
 public class SendMailService {
+    /**
+     * {@code mailSender} describes the JavaMailSender to inject on this
+     * class
+     */
     @Autowired
     private JavaMailSender mailSender;
+
+    /**
+     * {@code simpleMailMessage} describes the SimpleMailMessage to inject on this
+     * class
+     */
     @Autowired
     private SimpleMailMessage simpleMailMessage;
 
+    /**
+     * {@code request} describes the HttpServletRequest to inject on this
+     * class
+     */
+    @Autowired
+    private HttpServletRequest request;
+
+
+    /**
+     * {@method setSimpleMailMessage(SimpleMailMessage simpleMailMessage)}
+     * Inject in this class
+     */
     public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
         this.simpleMailMessage = simpleMailMessage;
     }
 
+    /**
+     * {@method setMailSender(JavaMailSender mailSender)}
+     * Inject in this class
+     *
+     */
     public void setMailSender(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
-
+    /**
+     * {@method setRequest(HttpServletRequest request)}
+     * Inject in this class
+     *
+     */
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+    /**
+     * {@method sendMail(String hash, String login, String email)}
+     * for send mail message
+     *
+     * @param hash (object of the hash)
+     * @param login (object of the Account.login)
+     * @param email (object of the Account.email)
+     *
+     */
     public void sendMail(String hash, String login, String email) {
 
-        SimpleMailMessage message1 = new SimpleMailMessage(simpleMailMessage);
-        message1.setText(String.format(simpleMailMessage.getText(),login, hash ));
-        message1.setTo(email);
+        StringBuffer requestURL = request.getRequestURL();
+        requestURL.delete(requestURL.lastIndexOf("forgotPassword"), requestURL.length());
+        String commonURL = requestURL.toString();
+        requestURL.append("changeForgottenPassword?hash=");
+        requestURL.append(hash);
+        String responseURL = requestURL.toString();
 
-        mailSender.send(message1);
+        Map<String,String> datamodel= new HashMap<String,String>();
+        datamodel.put("responseURL",responseURL);
+        datamodel.put("commonURL",commonURL);
+        datamodel.put("login",login);
 
+        Configuration   cfg = new Configuration();
 
+        cfg.setServletContextForTemplateLoading(request.getSession().getServletContext(),"WEB-INF/freemarker");
+
+        try {
+            Template temp = cfg.getTemplate("mail.ftl");
+            StringWriter writer = new StringWriter();
+            temp.process(datamodel,writer);
+
+            MimeMessage message1 = mailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(message1, true);
+
+            helper.setFrom(simpleMailMessage.getFrom());
+            helper.setTo(email);
+            helper.setSubject(simpleMailMessage.getSubject());
+            helper.setText(writer.toString(),true);
+            mailSender.send(message1);
+        }
+        catch (MessagingException e) {
+            throw new MailParseException(e);
+        }
+        catch (TemplateException e ) {
+
+        }
+        catch (IOException e ) {
 
         }
 
+    }
 
 }
