adcffb4b8cd1328b046fa1dc7ee567dda099b7b8
Elena Vasilkovich <vev77777@gmail.com>
2013-04-11 12:50:52 +0300

    Task 1086 Send Mail. Remove unused import from SendMailServiceImpl.

--- src/main/java/com/exigen/common/service/SendMailServiceImpl.java
+++ src/main/java/com/exigen/common/service/SendMailServiceImpl.java
@@ -5,7 +5,7 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailParseException;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
