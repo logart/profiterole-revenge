a09ea2729d01196e8f8ace375bdf7f48630aa599
gioia <gioia@localhost>
2013-06-06 23:25:15 +0300

    Task 1197

---
+++ src/main/java/com/exigen/common/web/AccessDeniedController.java
@@ -0,0 +1,35 @@
+package com.exigen.common.web;
+
+import com.exigen.common.domain.Account;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Class  {@code AccessDeniedController} is used to handle
+ *  requests about 403 exception, when ROLE_INACTIVE_USER(user without activation) try to go on pages:
+ *  /addRecipes, /editProfile, /profile and "forward" to the specified error page view.
+ *
+ * @autor Victoria Ganzha
+ * @Date 05.06.13
+ *
+ */
+
+@Controller
+@RequestMapping("/noaccess")
+public class AccessDeniedController {
+
+    @RequestMapping
+    public String accessDenied(Authentication authentication, Model model){
+        if(authentication.getAuthorities().contains(new SimpleGrantedAuthority(Account.ROLE_INACTIVE_USER))){
+            model.addAttribute("user", authentication.getName());
+            return "activationAccountNotFinished";
+
+        } else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority(Account.ROLE_USER))){
+            return "redirect:/";
+        }
+        return "redirect:/login";
+    }
+}
