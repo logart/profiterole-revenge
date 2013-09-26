ae7a5e0b738d80e9ff44234a2a09a7e2be06f3b5
gioia <gioia@leela>
2013-09-26 21:39:45 +0300

    Fix bug and sonar

--- src/main/java/com/exigen/common/domain/RoleConstants.java
+++ src/main/java/com/exigen/common/domain/RoleConstants.java
@@ -10,6 +10,10 @@ package com.exigen.common.domain;
  */
 public final class RoleConstants {
 
+    private RoleConstants(){
+
+    }
+
     public static final  String ROLE_ADMIN ="ROLE_ADMIN";
     public static final  String ROLE_USER ="ROLE_USER";
     public static final  String ROLE_INACTIVE_USER = "ROLE_INACTIVE_USER";
